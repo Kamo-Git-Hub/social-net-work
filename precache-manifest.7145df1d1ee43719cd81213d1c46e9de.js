b574620966a5ee25d2b71702ab9dab4050afe255
Kamo-Git-Hub <53765597+Kamo-Git-Hub@users.noreply.github.com>
2020-01-09 16:55:16 +0400

    Updates

---
+++ precache-manifest.7145df1d1ee43719cd81213d1c46e9de.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3fa01d0b2045b011fce9469cf4d6d16",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9601da3c4ae17d20da2e",
+    "url": "/static/css/3.fb1c0e25.chunk.css"
+  },
+  {
+    "revision": "24a6dab9af160bfd98cb",
+    "url": "/static/css/4.b3247331.chunk.css"
+  },
+  {
+    "revision": "626ea40fad8cb53eee6c",
+    "url": "/static/css/5.0445c5bf.chunk.css"
+  },
+  {
+    "revision": "50dae99ca3c0d78ea1f8",
+    "url": "/static/css/main.631c7edc.chunk.css"
+  },
+  {
+    "revision": "687a4eeac5bc639dcf6d",
+    "url": "/static/js/2.685490c3.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.685490c3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9601da3c4ae17d20da2e",
+    "url": "/static/js/3.06fb93b7.chunk.js"
+  },
+  {
+    "revision": "24a6dab9af160bfd98cb",
+    "url": "/static/js/4.c4866f2d.chunk.js"
+  },
+  {
+    "revision": "626ea40fad8cb53eee6c",
+    "url": "/static/js/5.fb056cc6.chunk.js"
+  },
+  {
+    "revision": "50dae99ca3c0d78ea1f8",
+    "url": "/static/js/main.1ae1f98c.chunk.js"
+  },
+  {
+    "revision": "87a994b504a945271471",
+    "url": "/static/js/runtime-main.40836315.js"
+  },
+  {
+    "revision": "ef474d09f00787104084b05ef0df9c9e",
+    "url": "/static/media/preloader.ef474d09.gif"
+  },
+  {
+    "revision": "8d867d5255e5d9e9bd979b7a9db5258a",
+    "url": "/static/media/right.8d867d52.png"
+  }
+]);
